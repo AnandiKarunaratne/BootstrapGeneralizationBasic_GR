a3354a03ff97d39b3f88aa43ee6ec44c84ae259b
Anandi Karunaratne <anandi.karunaratne@gmail.com>
2023-12-14 10:10:31 +1100

    modify code with stdDev

--- src/main/java/org/bootstrap/BootstrapGen.java
+++ src/main/java/org/bootstrap/BootstrapGen.java
@@ -1,7 +1,5 @@
 package org.bootstrap;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.math3.util.Pair;
 import org.bootstrap.gen.Generalization;
 import org.bootstrap.log.EventLog;
@@ -9,9 +7,8 @@ import org.bootstrap.lsm.LogBreeding;
 import org.bootstrap.lsm.LogSamplingMethod;
 import org.bootstrap.utils.LogUtils;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BootstrapGen {
     private final int sampleSize;
@@ -19,28 +16,80 @@ public class BootstrapGen {
     private final List<String> log;
     private final LogSamplingMethod logSamplingMethod;
     private final String modelFilePath;
-    private static String staticModelFilePath = "/Users/anandik/OneDrive - The University of Melbourne/GR/Experiments/LogQualityV3/model.pnml";
     private static String sampleFilePath = "/Users/anandik/OneDrive - The University of Melbourne/GR/Code/BootstrapGeneralizationBasic_GR/src/main/java/org/bootstrap/resources/samplelogfile.xes";
+    private static int g;
 
-
-    public BootstrapGen(int sampleSize, int numberOfSamples, EventLog eventLog, LogSamplingMethod logSamplingMethod, String modelFilePath) {
+    public BootstrapGen(int sampleSize, int numberOfSamples, EventLog eventLog, LogSamplingMethod logSamplingMethod, String modelFilePath, int g) {
         this.sampleSize = sampleSize;
         this.numberOfSamples = numberOfSamples;
         this.log = eventLog.getTraceList();
         this.logSamplingMethod = logSamplingMethod;
         this.modelFilePath = modelFilePath;
+        this.g = g;
+    }
+
+    public double[] calc() throws Exception {
+        Pair<Double, Double> genValueOfSample;
+        List<Pair<Double, Double>> genValues = new ArrayList<>();
+
+        Generalization generalization = new Generalization();
+        List<String> estimatedPopulation = estimatePopulation();
+
+        int uniqueTraceCount = 0;
+        int numOfSamples = 0;
+
+        double stdDevHighest = 10;
+
+        while (stdDevHighest > 0.1) {
+            List<String> sample = generateSample(estimatedPopulation);
+            numOfSamples++;
+
+            Set<String> distinctTraces = new HashSet<>(sample);
+            uniqueTraceCount += distinctTraces.size();
+
+            genValueOfSample = generalization.calculateEntropyRecallPrecision(modelFilePath, sampleFilePath);
+            genValues.add(genValueOfSample);
+
+            stdDevHighest = (genValues.size() >=2) ? calcHighestStdDev(genValues) : stdDevHighest;
+        }
+        List<Double> genValuesPre = genValues.stream().map(Pair::getValue).collect(Collectors.toList());
+        List<Double> genValuesRec = genValues.stream().map(Pair::getKey).collect(Collectors.toList());
+        double[] result = {calcMean(genValuesPre), calcStdDev(genValuesPre), calcMean(genValuesRec), calcStdDev(genValuesRec),
+                (double) numOfSamples, (double) uniqueTraceCount/numOfSamples};
+        return result;
+    }
+
+    private double calcHighestStdDev(List<Pair<Double, Double>> genValues) {
+        // recall
+        List<Double> recValues = genValues.stream().map(Pair::getKey).collect(Collectors.toList());
+        Double stdDevRec = calcStdDev(recValues);
+
+        // precision
+        List<Double> preValues = genValues.stream().map(Pair::getValue).collect(Collectors.toList());
+        Double stdDevPre = calcStdDev(preValues);
+
+        return (stdDevRec < stdDevPre) ? stdDevPre : stdDevRec;
+    }
+
+    private double calcStdDev(List<Double> values) {
+        double sum = 0.0;
+        for (double num : values) sum += Math.pow(num - calcMean(values), 2);
+        return Math.sqrt(sum / values.size());
+    }
+
+    private double calcMean(List<Double> values) {
+        double sum = 0.0;
+        for (double num : values) sum += num;
+        return sum / values.size();
     }
 
     /**
      * This method will calculate the bootstrap generalization value by aggregating the generalization value of samples
      */
-    public Double[] calculateBootstrapGen() throws Exception {
+    public double[] calculateBootstrapGen() throws Exception {
         Pair<Double, Double> genValueOfSample;
-        Pair<Double, Double> genValueOfStaticSample;
         double genValuesRecallSum = 0;
         double genValuesPrecisionSum = 0;
-        double genValuesRecallSumStatic = 0;
-        double genValuesPrecisionSumStatic = 0;
         Generalization generalization = new Generalization();
         List<String> estimatedPopulation = estimatePopulation();
 
@@ -58,12 +107,8 @@ public class BootstrapGen {
             genValueOfSample = generalization.calculateEntropyRecallPrecision(modelFilePath, sampleFilePath);
             genValuesRecallSum += genValueOfSample.getFirst();
             genValuesPrecisionSum += genValueOfSample.getSecond();
-
-            genValueOfStaticSample = generalization.calculateEntropyRecallPrecision(staticModelFilePath, sampleFilePath);
-            genValuesRecallSumStatic += genValueOfStaticSample.getFirst();
-            genValuesPrecisionSumStatic += genValueOfStaticSample.getSecond();
         }
-        Double[] result = {genValuesPrecisionSum/numberOfSamples, genValuesRecallSum/numberOfSamples, genValuesPrecisionSumStatic/numberOfSamples, genValuesRecallSumStatic/numberOfSamples, (double) uniqueTraceCount/numberOfSamples};
+        double[] result = {genValuesPrecisionSum/numberOfSamples, genValuesRecallSum/numberOfSamples, (double) uniqueTraceCount/numberOfSamples};
         return result;
     }
 
@@ -76,7 +121,7 @@ public class BootstrapGen {
         if (logSamplingMethod == LogSamplingMethod.NON_PARAMETRIC) {
             return log;
         } else if (logSamplingMethod == LogSamplingMethod.SEMI_PARAMETRIC) {
-            return new LogBreeding().estimatePopulationWithLogBreeding(log, 100, 2, 1.0);
+            return new LogBreeding().estimatePopulationWithLogBreeding(log, g, 2, 1.0);
         } else return null;
     }
 
--- src/main/java/org/bootstrap/Main.java
+++ src/main/java/org/bootstrap/Main.java
@@ -7,40 +7,49 @@ import org.bootstrap.lsm.LogSamplingMethod;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     final private static int numberOfSamples = 100; // m
+    final private static int g = 100;
     final private static LogSamplingMethod logSamplingMethod = LogSamplingMethod.SEMI_PARAMETRIC;
 
     public static void main(String[] args) throws Exception {
 
-        int[] logSizes = {100, 500, 1000, 5000, 10000};
+        int[] logSizes = {64, 128, 256, 512, 1024};
         String[] noiseTypes = {"oa", "ia", "ooo"};
+        double[] noiseLevels = {0, 0.25, 0.5, 0.75, 1, 5, 10, 15, 20};
 
-        for (int log = 0; log < 10; log++) {
+        for (int log = 0; log < 3; log++) {
             for (String noiseType : noiseTypes) {
                 for (int logSize : logSizes) {
                     int sampleSize = 5 * logSize;
-                    for (int noise = 0; noise < 21; noise = noise + 5) {
-                        String logFilePath = "/Users/anandik/OneDrive - The University of Melbourne/GR/Experiments/LogQualityV3/Log" + log + "/" + getFolder(noiseType) + "/Logs/log-" + logSize + "-" + noise + ".xes";
-                        String modelFilePath = "/Users/anandik/OneDrive - The University of Melbourne/GR/Experiments/LogQualityV3/Log" + log + "/" + getFolder(noiseType) + "/Models/model-" + logSize + "-" + noise + ".pnml";
+                    for (double noise : noiseLevels) {
+                        String logFilePath = "/Users/anandik/OneDrive - The University of Melbourne/GR/Experiments/LogQualityV4/Log" + log + "/" + getFolder(noiseType) + "/Logs/log-" + logSize + "-" + noise + ".xes";
+                        String modelFilePath = "/Users/anandik/OneDrive - The University of Melbourne/GR/Experiments/LogQualityV4/Log" + log + "/" + getFolder(noiseType) + "/Models/model-" + logSize + "-" + noise + ".pnml";
                         EventLog eventLog = new EventLog(logFilePath);
-                        Double[] result = new BootstrapGen(sampleSize, numberOfSamples, eventLog, logSamplingMethod, modelFilePath).calculateBootstrapGen();
-                        writeToCSV(log, logSize, noiseType, noise, result[0], result[1], result[2], result[3], result[4]);
+                        double[] result = new BootstrapGen(sampleSize, numberOfSamples, eventLog, logSamplingMethod, modelFilePath, g).calc();
+                        writeToCSV(String.valueOf(log), logSize, noiseType, noise, result);
                     }
                 }
             }
         }
     }
 
-    private static void writeToCSV(int log, int logSize, String noiseType, int noiseLevel, double precision, double recall, double staticPrec, double staticRec, double traces) {
-        String filePath = "/Users/anandik/OneDrive - The University of Melbourne/GR/Code/BootstrapGeneralizationBasic_GR/src/main/java/org/bootstrap/resources/output_constg.csv";
+    private static void writeToCSV(String log, int logSize, String noiseType, double noiseLevel, double[] result) {
+        String filePath = "/Users/anandik/OneDrive - The University of Melbourne/GR/Code/BootstrapGeneralizationBasic_GR/src/main/java/org/bootstrap/resources/bResultsStdDevTest.csv";
 
         try (FileWriter fileWriter = new FileWriter(filePath, true);
              CSVPrinter csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT)) {
-            String[] values = {Integer.toString(log), Integer.toString(logSize), noiseType, Integer.toString(noiseLevel), Double.toString(precision), Double.toString(recall), Double.toString(staticPrec), Double.toString(staticRec), Double.toString(traces)};
-            csvPrinter.printRecord(Arrays.asList(values));
+            List<String> values = new ArrayList<>();
+            values.add(log);
+            values.add(Integer.toString(logSize));
+            values.add(noiseType);
+            values.add(Double.toString(noiseLevel));
+            for (double res : result) values.add(Double.toString(res));
+            csvPrinter.printRecord(values);
         } catch (IOException e) {
             e.printStackTrace();
         }
